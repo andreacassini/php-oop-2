e7c45060876eb2b205a2906d39d5ab9cb26e3b7a
andreacassini <andrea1316@hotmail.it>
2023-07-15 17:58:31 +0200

    Importo classi nel file index

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-
+//IMPORTO CLASSI
+require_once __DIR__ . "./Models/Prodotti.php";
+require_once __DIR__ . "./Models/Giochi.php";
+require_once __DIR__ . "./Models/Cibo.php";
+require_once __DIR__ . "./Models/Accessori.php";
 ?>
 
 <!DOCTYPE html>
