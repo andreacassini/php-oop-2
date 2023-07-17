7fc097b112d27405211a597176024e46fbcf19f6
andreacassini <andrea1316@hotmail.it>
2023-07-17 12:47:17 +0200

    ADD costruttore accessori.php & cibo.php

--- Models/Accessori.php
+++ Models/Accessori.php
@@ -4,4 +4,11 @@ class Accessori extends Prodotti
     //PROPRIETA'
     private $materials;
     private $sizes;
+
+    function __construct($image, $title, $category, $price, $materials, $sizes)
+    {
+        parent::__construct($image, $title, $category, $price);
+        $this->materials = $materials;
+        $this->sizes = $sizes;
+    }
 }
--- Models/Cibo.php
+++ Models/Cibo.php
@@ -4,4 +4,11 @@ class Cibo extends Prodotti
     //PROPRIETA'
     private $ingredients;
     private $weight;
+
+    function __construct($image, $title, $category, $price, $ingredients, $weight)
+    {
+        parent::__construct($image, $title, $category, $price);
+        $this->ingredients = $ingredients;
+        $this->weight = $weight;
+    }
 }
