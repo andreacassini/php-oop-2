0456944a36da8ee4f14b151874f934fba235bc94
andreacassini <andrea1316@hotmail.it>
2023-07-17 12:44:32 +0200

    Add costruttore giochi.php

--- Models/Giochi.php
+++ Models/Giochi.php
@@ -4,4 +4,11 @@ class Giochi extends Prodotti
     //PROPRIETA'
     private $features;
     private $sizes;
+
+    function __construct($image, $title, $category, $price, $features, $sizes)
+    {
+        parent::__construct($image, $title, $category, $price);
+        $this->features = $features;
+        $this->sizes = $sizes;
+    }
 }
